87fdf0d237af3fa473ff10940af27dfa856f965b
AyanC1 <choudhury.ayan13@gmail.com>
2020-02-22 04:36:55 +0000

    Revert " SystemUI: Implement alert slider UI"
    This reverts commit abbe0be0b5a6f8f9090503bff6899f3025249da9.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogComponent.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogComponent.java
@@ -28,8 +28,6 @@ import android.view.WindowManager.LayoutParams;
 import com.android.settingslib.applications.InterestingConfigChanges;
 import com.android.systemui.Dependency;
 import com.android.systemui.SystemUI;
-import com.android.systemui.TriStateUiController;
-import com.android.systemui.TriStateUiControllerImpl;
 import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.PluginDependencyProvider;
@@ -46,7 +44,7 @@ import java.io.PrintWriter;
  * Implementation of VolumeComponent backed by the new volume dialog.
  */
 public class VolumeDialogComponent implements VolumeComponent, TunerService.Tunable,
-        VolumeDialogControllerImpl.UserActivityListener, TriStateUiController.UserActivityListener {
+        VolumeDialogControllerImpl.UserActivityListener{
 
     public static final String VOLUME_DOWN_SILENT = "sysui_volume_down_silent";
     public static final String VOLUME_UP_SILENT = "sysui_volume_up_silent";
@@ -59,7 +57,6 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
     private final SystemUI mSysui;
     protected final Context mContext;
     private final VolumeDialogControllerImpl mController;
-    private TriStateUiControllerImpl mTriStateController;
     private final InterestingConfigChanges mConfigChanges = new InterestingConfigChanges(
             ActivityInfo.CONFIG_FONT_SCALE | ActivityInfo.CONFIG_LOCALE
             | ActivityInfo.CONFIG_ASSETS_PATHS | ActivityInfo.CONFIG_UI_MODE);
@@ -76,8 +73,6 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
         mContext = context;
         mController = (VolumeDialogControllerImpl) Dependency.get(VolumeDialogController.class);
         mController.setUserActivityListener(this);
-        boolean hasAlertSlider = mContext.getResources().
-                getBoolean(com.android.internal.R.bool.config_hasAlertSlider);
         // Allow plugins to reference the VolumeDialogController.
         Dependency.get(PluginDependencyProvider.class)
                 .allowPluginDependency(VolumeDialogController.class);
@@ -90,13 +85,6 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
                     }
                     mDialog = dialog;
                     mDialog.init(LayoutParams.TYPE_VOLUME_OVERLAY, mVolumeDialogCallback);
-                    if (hasAlertSlider) {
-                        if (mTriStateController != null) {
-                            mTriStateController.destroy();
-                        }
-                        mTriStateController = new TriStateUiControllerImpl(mContext);
-                        mTriStateController.init(LayoutParams.TYPE_VOLUME_OVERLAY, this);
-                    }
                 }).build();
         applyConfiguration();
         Dependency.get(TunerService.class).addTunable(this, VOLUME_DOWN_SILENT, VOLUME_UP_SILENT,
@@ -186,10 +174,6 @@ public class VolumeDialogComponent implements VolumeComponent, TunerService.Tuna
     private void startSettings(Intent intent) {
         Dependency.get(ActivityStarter.class).startActivity(intent,
                 true /* onlyProvisioned */, true /* dismissShade */);
-    }
-    @Override
-    public void onTriStateUserActivity() {
-        onUserActivity();
     }
 
     private final VolumeDialogImpl.Callback mVolumeDialogCallback = new VolumeDialogImpl.Callback() {
