e82de907556e0113000ee3c48a9136f2c7614dfe
AyanC1 <choudhury.ayan13@gmail.com>
2020-03-05 18:09:42 +0000

    Revert "SystemUI: Add statusbar NFC icon"
    This reverts commit ae830c3b1a3a04fd8397774129052b00f9ae1635.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -29,7 +29,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
-import android.nfc.NfcAdapter;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
@@ -111,7 +110,6 @@ public class PhoneStatusBarPolicy
     private final String mSlotMicrophone;
     private final String mSlotCamera;
     private final String mSlotSensorsOff;
-    private final String mSlotNfc;
 
     private final Context mContext;
     private final Handler mHandler = new Handler();
@@ -131,8 +129,6 @@ public class PhoneStatusBarPolicy
     private final PrivacyItemController mPrivacyItemController;
     private final UiOffloadThread mUiOffloadThread = Dependency.get(UiOffloadThread.class);
     private final SensorPrivacyController mSensorPrivacyController;
-    private boolean mNfcVisible;
-    private NfcAdapter mAdapter;
 
     // Assume it's all good unless we hear otherwise.  We don't always seem
     // to get broadcasts that it *is* there.
@@ -182,7 +178,6 @@ public class PhoneStatusBarPolicy
         mSlotMicrophone = context.getString(com.android.internal.R.string.status_bar_microphone);
         mSlotCamera = context.getString(com.android.internal.R.string.status_bar_camera);
         mSlotSensorsOff = context.getString(com.android.internal.R.string.status_bar_sensors_off);
-        mSlotNfc = context.getString(com.android.internal.R.string.status_bar_nfc);
 
         // listen for broadcasts
         IntentFilter filter = new IntentFilter();
@@ -195,7 +190,6 @@ public class PhoneStatusBarPolicy
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_AVAILABLE);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_UNAVAILABLE);
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED);
-        filter.addAction(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED);
         mContext.registerReceiver(mIntentReceiver, filter, null, mHandler);
 
         // listen for user / profile change.
@@ -259,12 +253,6 @@ public class PhoneStatusBarPolicy
                 mContext.getString(R.string.accessibility_sensors_off_active));
         mIconController.setIconVisibility(mSlotSensorsOff,
                 mSensorPrivacyController.isSensorPrivacyEnabled());
-
-        mIconController.setIcon(mSlotNfc, R.drawable.stat_sys_nfc,
-                mContext.getString(R.string.accessibility_status_bar_nfc));
-
-        mIconController.setIconVisibility(mSlotNfc, false);
-        updateNfc();
 
         mRotationLockController.addCallback(this);
         mBluetooth.addCallback(this);
@@ -343,26 +331,6 @@ public class PhoneStatusBarPolicy
             }
         } else {
             mSimState = IccCardConstants.State.UNKNOWN;
-        }
-    }
-
-    private NfcAdapter getAdapter() {
-        if (mAdapter == null) {
-            try {
-                mAdapter = NfcAdapter.getNfcAdapter(mContext);
-            } catch (UnsupportedOperationException e) {
-                mAdapter = null;
-            }
-        }
-        return mAdapter;
-    }
-
-    private final void updateNfc() {
-        mNfcVisible =  getAdapter() != null && getAdapter().isEnabled();
-        if (mNfcVisible) {
-            mIconController.setIconVisibility(mSlotNfc, true);
-        } else {
-            mIconController.setIconVisibility(mSlotNfc, false);
         }
     }
 
@@ -787,9 +755,6 @@ public class PhoneStatusBarPolicy
                     break;
                 case BluetoothDevice.ACTION_BATTERY_LEVEL_CHANGED:
                     updateBluetooth();
-                    break;
-                case NfcAdapter.ACTION_ADAPTER_STATE_CHANGED:
-                    updateNfc();
                     break;
             }
         }
